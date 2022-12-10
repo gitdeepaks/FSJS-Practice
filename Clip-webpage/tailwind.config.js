1c37c4528b08b7cc20692d65609925e786d61cfc
gitdeepaks <100864803+gitdeepaks@users.noreply.github.com>
2022-12-10 13:13:28 +0530

    Clipwebpage side

---
+++ Clip-webpage/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['*'],
+  theme: {
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayindhBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      }
+    },
+  },
+  plugins: [],
+}
