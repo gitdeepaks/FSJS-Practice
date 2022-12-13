ed69c64bbee866b221d2595c79a4a5d522b40216
gitdeepaks <deepaksankhyan92@outlook.com>
2022-12-13 19:24:51 +0530

    js practice

---
+++ 04 Javascript-Practice/app.js
@@ -0,0 +1,66 @@
+// // alert('Hello World!');
+
+// // Log to console
+// console.log('Hello World!');
+// console.log(123)
+// console.log(true)
+// var greeting = 'Hello World!'
+// console.log(greeting)
+// console.log([1,2,3,4,5,6])
+// console.table({greeting})
+// console.clear();
+// console.warn("this is a warning message");
+
+
+// // var,let,const
+
+
+// var name = "John Doe";
+// console.log(name);
+// var name = "Steve Smith";
+// console.log(name);
+
+
+
+// // INit var
+
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello !';
+
+
+// //letters, numbers, and _ and $
+// //canoot startv the number  
+
+
+// // multiword vars
+
+// var firstName = "John Doe"; //Camebnle case when
+
+// var first_name = "Steve Smith";//UnderScore
+// var FirstName = 'Tom';//Pascal case
+// var firstName;
+
+
+
+// let name;
+// const name = "John Doe";
+// console.log(name);
+// // /cant do reassign name
+
+// // /Have to assign a value
+
+
+const person= {    
+    name: 'John Doe'
+}
+console.log(person);
+
+person.name = 'Sara';
+console.log(person);
+
+// Array
+
+const numbers = [1,2,3,4,5,6,7];
+numbers.push(4);
+console.log(numbers);
