4d90472820d1424be05223dc52c019a147ad5d66
gitdeepaks <deepaksankhyan92@outlook.com>
2022-12-17 20:48:47 +0530

    adding strings

---
+++ Strings/app.js
@@ -0,0 +1,86 @@
+const fName = "Jhon";
+const lName = "Deo";
+
+const str = "Hello there this is Deepak";
+
+let val;
+
+val = fName+lName;
+
+// // Concatenation
+// val = fName + " "+ lName;
+// // Append
+
+// val = "Hitessh"
+// val += "Choudhary"
+
+// val = 'Hello my name is ' + fName + '& I am ' + lName ;
+
+
+// // Escaping
+
+// val = "'That 's awesome, I Can't wait";
+
+
+// // length
+
+// val = fName.length;
+
+// // concat
+
+// val =firstname.concat(' ',lName);
+
+
+// // Change case
+
+// val = fName.toUpperCase();
+// val = fName.toLowerCase();
+
+
+// val =fName[2];
+
+
+// // indexOf()
+
+// val = fName.indexOf('n');
+// val = fName.lastIndexOf('n');
+
+
+// carAt
+
+val = fName.charAt('2');
+//  get last character
+
+
+
+
+val= fName.charAt(fNames.length -1);
+
+// substreings
+
+val = fName.substring(0,3);
+
+
+// /slice
+
+
+val =fName.slice(0,3);
+val =fName.slice(-3);
+
+// SPLIT
+
+val =str.split('');
+
+val =tag.split(',');
+
+
+// replace
+
+val = str.replace('Deepak', 'Brad');
+
+// includes
+
+
+val =str.includes('food')
+
+console.log(val);
