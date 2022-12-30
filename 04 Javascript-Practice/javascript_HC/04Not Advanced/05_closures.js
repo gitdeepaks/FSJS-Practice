7f74dd3162fa1628580a7b42a9cfef055944adb5
gitdeepaks <deepaksankhyan92@outlook.com>
2022-12-30 21:27:58 +0530

    adding practice file 5

---
+++ 04 Javascript-Practice/javascript_HC/04Not Advanced/05_closures.js
@@ -0,0 +1,27 @@
+// function init() {
+//     var firstName = "deepak";
+//     console.log("i am init");
+//     function sayFirstName() {
+//       console.log(firstName);
+      
+//     }
+//     return sayFirstName;
+//   }
+  
+  
+//   var  value = init();  
+
+//   value();
+
+
+function doAddition(x) {
+    return function(y){
+        return x+y;
+    }
+}
+
+var add5 = doAddition(4);
+console.log(add5(5)); 
+
+
+console.log(doAddition(5)(5));
