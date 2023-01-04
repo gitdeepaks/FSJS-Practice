ce78177020b608369d395b20a891e285d35bc9f4
gitdeepaks <deepaksankhyan92@outlook.com>
2023-01-04 18:05:14 +0530

    Adding Project 18

---
+++ 04 Javascript-Practice/50 Projects HTML,CSS&Javascript/18_Background Slider/script.js
@@ -0,0 +1,48 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const righBtn = document.getElementById('right');
+
+
+let activeSlide = 0
+
+righBtn.addEventListener('click', () => {
+    activeSlide++;
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--;
+
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1;
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+
+    } )
+
+    slides[activeSlide].classList.add('active')
+    
+}
